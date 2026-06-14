d6f66db0e92cec27efaf2a88c5190e0470d09ece
rpolubinskii <rodionpolubinskii@gmail.com>
2026-06-14 23:13:07 +0300

    rgbd camera settings change

--- ros2_ws/bringup/launch/diffbot.launch.py
+++ ros2_ws/bringup/launch/diffbot.launch.py
@@ -286,8 +286,8 @@ def generate_launch_description():
             'enable_gyro': 'true',
             'enable_accel': 'true',
             'unite_imu_method': '2',
-            'rgb_camera.color_profile': '640x360x30',
-            'depth_module.depth_profile': '640x360x30',
+            'rgb_camera.color_profile': '320x180x60',
+            'depth_module.depth_profile': '320x180x60',
         }.items(),
     )
 
